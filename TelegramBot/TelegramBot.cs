4b07f0dcd16fd3127e4222acadcd4734c298865a
Matteo <mattcheru03@gmail.com>
2022-07-19 11:49:39 +0200

    a

--- TelegramBot/TelegramBot.cs
+++ TelegramBot/TelegramBot.cs
@@ -208,7 +208,7 @@ namespace TelegramBot
 
             InlineKeyboardButton[][] Rows = new InlineKeyboardButton[2][];
 
-            for (int i = 0; i < 2; i += 4)
+            for (int i = 0; i < 2; i ++)
             {
                 int len = i == 0 ? 4 : 1;
                 var currentLine = new InlineKeyboardButton[len];
