6b8579357e82d57bde0f0c8a7cb2d134ef1e1f84
Matteo <mattcheru03@gmail.com>
2022-10-27 16:18:54 +0200

    a

--- Utility/HardwareDriver.cs
+++ Utility/HardwareDriver.cs
@@ -106,7 +106,7 @@ namespace Utility
                     var data = Enumerable.Repeat(target.GetAddressBytes(), 16).SelectMany(mac => mac);
                     var magicPacket = header.Concat(data).ToArray();
                     using var client = new UdpClient();
-                    client.Send(magicPacket, magicPacket.Length, new IPEndPoint(IPAddress.Any, 9));
+                    client.Send(magicPacket, magicPacket.Length, new IPEndPoint(IPAddress.Parse("169.254.255.255"), 9));
 
                     return "PC in accensione";
                 }
