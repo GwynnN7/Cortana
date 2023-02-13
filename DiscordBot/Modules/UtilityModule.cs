0817266f784c2ea8b49457b812b266df9e8ac456
Matteo Cherubini <mattcheru03@gmail.com>
2023-02-13 17:07:39 +0100

    test

--- DiscordBot/Modules/UtilityModule.cs
+++ DiscordBot/Modules/UtilityModule.cs
@@ -411,14 +411,14 @@ namespace DiscordBot.Modules
                     GameEmbed = GameEmbed.ToEmbedBuilder()
                         .WithDescription($"[Vai alla pagina IGDB]({foundGame.Url})")
                         .WithThumbnailUrl($"https://images.igdb.com/igdb/image/upload/t_cover_big/{foundGame.Cover.Value.ImageId}.jpg")
-                        //.AddField("Game Engine", foundGame.GameEngines.Values.First().Name, inline: true)
-                        .AddField("Genres", string.Join("\n", foundGame.Genres.Values.Take(3).Select(x => x.Name)), inline: true)
-                        .AddField("Developers", string.Join("\n", foundGame.InvolvedCompanies.Values.Take(3).Select(x => x.Company.Value.Name)), inline: true)
-                        .AddField("Platforms", string.Join("\n", foundGame.Platforms.Values.Take(3).Select(x => x.Name)), inline: true)
-                        //.AddField("Rating", foundGame.Rating.ToString() ?? "Unknown", inline: true)
-                        .AddField("Release Date", foundGame.ReleaseDates.Values.First().Human, inline: true)
-                        .AddField("Themes", string.Join("\n", foundGame.Themes.Values.Take(3).Select(x => x.Name)), inline: true)
-                        .WithFooter(foundGame.Summary.Take(350).ToString() + "...")
+                        .AddField("Game Engine", foundGame.GameEngines.Values.First().Name)
+                        .AddField("Genres", string.Join("\n", foundGame.Genres.Values.Take(3).Select(x => x.Name)))
+                        .AddField("Developers", string.Join("\n", foundGame.InvolvedCompanies.Values.Take(3).Select(x => x.Company.Value.Name)))
+                        .AddField("Platforms", string.Join("\n", foundGame.Platforms.Values.Take(3).Select(x => x.Name)))
+                        .AddField("Rating", foundGame.Rating ? foundGame.Rating.ToString() : "Unknown")
+                        .AddField("Release Date", foundGame.ReleaseDates.Values.First().Human)
+                        .AddField("Themes", string.Join("\n", foundGame.Themes.Values.Take(3).Select(x => x.Name)))
+                        .WithFooter(foundGame.Summary.Substring(0, 350) + "...")
                         .Build();
                     await RespondAsync(embed: GameEmbed, ephemeral: Ephemeral == EAnswer.Si);
 
