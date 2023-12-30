b87ec783c8178ea87a4f2434a041f2258fea6a46
GwynbleiddN7 <mattcheru03@gmail.com>
2023-12-30 19:06:15 +0100

    Test

--- CortanaAPI/CortanaAPI.cs
+++ CortanaAPI/CortanaAPI.cs
@@ -11,7 +11,7 @@ namespace CortanaAPI
         private static WebApplication? CortanaWebAPI;
         public static void BootCortanaAPI()
         {
-            var builder = WebApplication.CreateBuilder(new[] { $"--urls=http://{HardwareDriver.NetStats.Cortana_IP}:8080/" });
+            var builder = WebApplication.CreateBuilder(new[] { $"--urls=https://localhost::8080/" });
 
             Assembly RequestsHandlerAssemby = Assembly.Load(new AssemblyName("CortanaAPI"));
             builder.Services.AddMvc().AddApplicationPart(RequestsHandlerAssemby);
