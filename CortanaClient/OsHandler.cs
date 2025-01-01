592d400ab3ef23756525cc0beb879b120de725a3
GwynbleiddN7 <mattcheru03@gmail.com>
2025-01-01 23:38:07 +0100

    Client modifications

--- CortanaClient/OsHandler.cs
+++ CortanaClient/OsHandler.cs
@@ -43,9 +43,9 @@ internal static class OsHandler
 		bool onLinux = OperatingSystem == Os.Linux;
 		return command switch
 		{
-			"shutdown" => onLinux ? "poweroff" : "shutdown /s /hybrid /f",
-			"reboot" => onLinux ? "reboot" : "shutdown /r /f",
-			"swap_os" => onLinux ? $"echo {ComputerClient.ClientInfo.ClientPassword} | sudo -S grub-reboot 1 && reboot" : "shutdown /r",
+			"shutdown" => onLinux ? "poweroff" : "shutdown /s /f /t 0",
+			"reboot" => onLinux ? "reboot" : "shutdown /r /f /t 0",
+			"swap_os" => onLinux ? $"echo {ComputerClient.ClientInfo.ClientPassword} | sudo -S grub-reboot 1 && reboot" : "shutdown /r /f /t 0",
 			"notify" => onLinux ? $"notify-send -u low -a Cortana \'{arg}\'" : $"notify-send \"Cortana\" \"{arg}\"",
 			"cmd" => onLinux ? $"echo {ComputerClient.ClientInfo.ClientPassword} | sudo -S {arg}" : arg,
 			_ => ""
--- CortanaClient/Scripts/cortana-linux.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/zsh
-
-export CORTANA_PATH=/home/gwynn7/Programming/Cortana
-
-nohup dotnet run --project $CORTANA_PATH/CortanaClient/CortanaClient.csproj > /dev/null 2> /dev/null & disown;
