44f6da7a5536f2bc82eaeae095cc16fdbe05d4cf
GwynbleiddN7 <mattcheru03@gmail.com>
2025-02-08 20:30:59 +0100

    Finished Web App

--- CortanaWeb/CortanaWebApp.cs
+++ CortanaWeb/CortanaWebApp.cs
@@ -1,4 +1,3 @@
-using CortanaLib;
 using CortanaWeb.Components;
 
 namespace CortanaWeb;
@@ -6,29 +5,20 @@ namespace CortanaWeb;
 public class CortanaWebApp
 {
     public static async Task Main(string[] args)
-    {
+    { 
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-        
         builder.Services.AddLogging(c => c.ClearProviders());
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
         
-        builder.Services.AddScoped<HttpClient>(_ => new HttpClient { BaseAddress = new Uri(DataHandler.Env("CORTANA_API")) });
- 
         WebApplication app = builder.Build();
         
         app.UseAntiforgery();
         app.MapStaticAssets();
         app.MapRazorComponents<App>()
             .AddInteractiveServerRenderMode();
+        await app.RunAsync();
 
-        Task webTask = Task.Run(async () => await app.RunAsync());
-        Task stopTask = Task.Run(async () =>
-        {
-            await SignalHandler.WaitForInterrupt();
-            await app.StopAsync();
-            await app.DisposeAsync();
-        });
-        await Task.WhenAll(webTask, stopTask);
+
     }
 }
